13b0c5bf84396fbbb39b64b751e6f59fbe30bb24
JulianSobott <julian.sobott@gmail.com>
2019-02-14 18:00:51 +0100

    Beautify BrowserStartPage

--- projects/BrowserStartPage/JS/main.js
+++ projects/BrowserStartPage/JS/main.js
@@ -1,24 +1,36 @@
 $(document).ready(function () {
 
   //Background
-  //changeBackgroundColor();
-
+  var num_images = 6;
+  var number = Math.floor(Math.random() * num_images + 1);
+  var image_number = "";
+  if (number < 10){
+    image_number = "0" + number;
+  }else{
+    image_number = "" + number;
+  }
   //------Welcome Textfield--------
   var date = new Date();
   var hour = date.getHours();
   var welcomeText = "";
   if(hour<12 && hour>4){
     welcomeText = "Guten Morgen";
+    document.body.style.backgroundImage = "url('img/backgrounds/morning_" + image_number + ".jpg')";
   }else if (hour>11 && hour<14) {
     welcomeText = "Guten Mittag"
+    document.body.style.backgroundImage = "url('img/backgrounds/day_" + image_number + ".jpg')";
   }else if (hour>13 && hour<17) {
     welcomeText = "Guten Nachmittag";
+    document.body.style.backgroundImage = "url('img/backgrounds/day_" + image_number + ".jpg')";
   }else if (hour>16 && hour<21) {
     welcomeText = "Guten Abend";
+    document.body.style.backgroundImage = "url('img/backgrounds/evening_" + image_number + ".jpg')";
   }else if (hour>20) {
     welcomeText = "Gute Nacht";
+    document.body.style.backgroundImage = "url('img/backgrounds/night_" + image_number + ".jpg')";
   }else {
     welcomeText = "Hello";
+    document.body.style.backgroundImage = "url('img/backgrounds/day_" + image_number + ".jpg')";
   }
   $('.welcomeText').html(welcomeText);
   $('.welcomeText').css('font-size', $('.welcomeText').height() / 2 - 20);
